4357727de9a96916b142db0cf4114be5e0ebfe30
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-06-20 17:08:17 +0700

    update day7

---
+++ day7/lab2.3.1.18.py
@@ -0,0 +1,11 @@
+def mysplit(strng):
+    #
+    # put your code here
+    return strng.split()
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
